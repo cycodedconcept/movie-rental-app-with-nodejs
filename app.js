f6badfc03dcefc5820621a501d39a91f011d28f3
cycodedconcept <cycodedconcept@gmail.com>
2023-09-17 07:46:48 +0100

    adding customer and rental models and route

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const home = require('./routes/home');
 const app = express();
 
@@ -12,6 +14,8 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json());
 app.use('/api/movies', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 app.use('/', home);
 
 const port = process.env.PORT || 5000;
