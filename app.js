031c0a069fb26fd04e445e855ac8413806214366
cycodedconcept <cycodedconcept@gmail.com>
2023-10-11 19:23:06 +0100

    bug fixed

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+
+
 const home = require('./routes/home');
 const app = express();
 
@@ -18,6 +22,9 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+
 app.use('/', home);
 
 const port = process.env.PORT || 5000;
